37c6018ebfbe9ec639f8d95e8a293785e0bdc188
Alban Voiriot <alban.voiriot@gmail.com>
2024-07-12 09:11:09 +0200

    Finish task about access rights and roles

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN', 403, message: 'Vous n\'avez pas les droits pour gérer les utilisateurs')]
 class UserController extends AbstractController
 {
     public function __construct(private readonly EntityManagerInterface $entityManager, private readonly UserPasswordHasherInterface $passwordHasher)
